430af687eed8588305a96be60a65a711c4a4c2fc
Mitutoyo <mitutoyum@proton.me>
2025-01-22 22:11:00 +0700

    Update

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,25 +1,13 @@
 local onedark = {
 	"navarasu/onedark.nvim",
 	priority = 1000,
-	config = function()
+	opts = {
+		style = "warmer",
+	},
+	config = function(_, opts)
 		local onedark = require("onedark")
-		onedark.setup({
-			style = "warmer",
-		})
+		onedark.setup(opts)
 		onedark.load()
-	end,
-}
-
-local catppuccin = {
-	"catppuccin/nvim",
-	name = "catppuccin",
-	priority = 1000,
-	config = function()
-		require("catppuccin").setup({
-			-- flavour = ''
-			transparent_background = true,
-		})
-		vim.cmd.colorscheme("catppuccin")
 	end,
 }
 
@@ -28,6 +16,18 @@ local onedarkpro = {
 	priority = 1000,
 	config = function()
 		vim.cmd.colorscheme("onedark")
+	end,
+}
+local catppuccin = {
+	"catppuccin/nvim",
+	name = "catppuccin",
+	priority = 1000,
+	opts = {
+		transparent_background = true,
+	},
+	config = function(_, opts)
+		require("catppuccin").setup(opts)
+		vim.cmd.colorscheme("catppuccin")
 	end,
 }
 
