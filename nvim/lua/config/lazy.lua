f7017ae15c9cb30eacfc28ffbe5653486f2a3504
Mitutoyo <mitutoyum@proton.me>
2025-01-22 22:14:33 +0700

    update

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -19,13 +19,10 @@ require("lazy").setup({
 		{ import = "plugins" },
 	},
 	defaults = {
-		-- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
-		-- If you know what you're doing, you can set this to `true` to have all your custom plugins lazy-loaded by default.
-		-- lazy = false,
-		-- It's recommended to leave version=false for now, since a lot the plugin that support versioning,
-		-- have outdated releases, which may break your Neovim install.
-		version = false, -- always use the latest git commit
+		version = nil, -- always use the latest git commit
 		-- version = "*", -- try installing the latest stable version for plugins that support semver
+
+		lazy = false,
 	},
 	checker = {
 		enabled = true, -- check for plugin updates periodically
@@ -36,7 +33,6 @@ require("lazy").setup({
 			enabled = true,
 		},
 		rtp = {
-			-- disable some rtp plugins
 			disabled_plugins = {
 				"gzip",
 				"matchit",
