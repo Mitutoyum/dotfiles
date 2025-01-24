77ed240cb0cb3d5120f1d8601c3b34212e419cd2
Mitutoyo <mitutoyum@proton.me>
2025-01-24 11:21:30 +0700

    small update

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -8,7 +8,11 @@ return {
 	},
 	config = function()
 		local lspconfig = require("lspconfig")
-		local default_capabilities = require("cmp_nvim_lsp").default_capabilities()
+		local capabilities = vim.tbl_deep_extend(
+			"force",
+			vim.lsp.protocol.make_client_capabilities(),
+			require("cmp_nvim_lsp").default_capabilities()
+		)
 
 		local opts = {
 			-- add lsp options here
@@ -26,6 +30,8 @@ return {
 		}
 
 		vim.api.nvim_create_autocmd("LspAttach", {
+
+			group = vim.api.nvim_create_augroup("lsp-attach", { clear = true }),
 			callback = function(event)
 				local builtin = require("telescope.builtin")
 
@@ -44,6 +50,36 @@ return {
 
 				map("ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
 				map("rn", vim.lsp.buf.rename, "[R]e[n]ame")
+
+				local client = vim.lsp.get_client_by_id(event.data.client_id)
+				if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight) then
+					local highlight_augroup = vim.api.nvim_create_augroup("lsp-highlight", { clear = false })
+					vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+						buffer = event.buf,
+						group = highlight_augroup,
+						callback = vim.lsp.buf.document_highlight,
+					})
+
+					vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
+						buffer = event.buf,
+						group = highlight_augroup,
+						callback = vim.lsp.buf.clear_references,
+					})
+
+					vim.api.nvim_create_autocmd("LspDetach", {
+						group = vim.api.nvim_create_augroup("lsp-detach", { clear = true }),
+						callback = function(event2)
+							vim.lsp.buf.clear_references()
+							vim.api.nvim_clear_autocmds({ group = "lsp-highlight", buffer = event2.buf })
+						end,
+					})
+				end
+
+				if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) then
+					map("<leader>th", function()
+						vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = event.buf }))
+					end, "[T]oggle Inlay [H]ints")
+				end
 			end,
 		})
 
@@ -53,7 +89,7 @@ return {
 					local opt = opts[ls] or {}
 
 					if not opt.capabilities then
-						opt.capabilities = default_capabilities
+						opt.capabilities = capabilities
 					end
 
 					lspconfig[ls].setup(opt)
