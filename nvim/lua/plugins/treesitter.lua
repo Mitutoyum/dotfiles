25f0b4c074d90dd98974667fe903f9ba793f6776
Mitutoyo <mitutoyum@proton.me>
2025-01-24 11:25:18 +0700

    removed bloatware

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,16 +1,26 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
 	verson = false,
-
 	build = function()
 		require("nvim-treesitter.install").update({ with_sync = true })()
 	end,
-
 	config = function()
 		require("nvim-treesitter.configs").setup({
-			-- ensure_installed = { "lua", "vim", "vimdocs", "query", "c", "cpp", "python", "markdown", "markdown_inline" },
+			ensure_installed = {
+				"lua",
+				"luadoc",
+				"vim",
+				"vimdoc",
+				"query",
+				"c",
+				"cpp",
+				"cmake",
+				"python",
+				"markdown",
+				"markdown_inline",
+			},
 			-- sync_install = true, -- for low-end devices
-			ensure_installed = "all",
+			-- ensure_installed = "all",
 			auto_install = true,
 			highlight = { enable = true },
 			indent = { enable = true },
