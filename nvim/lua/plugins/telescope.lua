e9e5f8150b11e7530a5388fb5294d1fd0f5c3234
Mitutoyo <mitutoyum@proton.me>
2025-01-24 11:25:03 +0700

    update

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -4,23 +4,23 @@ return {
 		"nvim-lua/plenary.nvim",
 		"nvim-telescope/telescope-ui-select.nvim",
 	},
-	config = function()
+	opts = {
+		extensions = {
+			["ui-select"] = {
+				require("telescope.themes").get_dropdown(),
+			},
+		},
+	},
+	config = function(_, opts)
 		local telescope = require("telescope")
 		local builtin = require("telescope.builtin")
-		local map = vim.keymap.set
 
-		telescope.setup({
-			extensions = {
-				["ui-select"] = {
-					require("telescope.themes").get_dropdown(),
-				},
-			},
-		})
+		telescope.setup(opts)
 		telescope.load_extension("ui-select")
 
-		map("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
-		map("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" }) -- needs ripgrep
-		map("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
-		map("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
+		vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
+		vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" }) -- needs ripgrep
+		vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
+		vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
 	end,
 }
