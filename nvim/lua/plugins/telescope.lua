309ffce052b24c4be78d8900329bd605a93a2a92
Mitutoyo <mitutoyum@proton.me>
2025-02-16 13:14:33 +0700

    Added file browser

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,26 +1,90 @@
 return {
 	"nvim-telescope/telescope.nvim",
+	-- lazy = true,
 	dependencies = {
 		"nvim-lua/plenary.nvim",
 		"nvim-telescope/telescope-ui-select.nvim",
-	},
-	opts = {
-		extensions = {
-			["ui-select"] = {
-				require("telescope.themes").get_dropdown(),
-			},
+		"nvim-telescope/telescope-file-browser.nvim",
+		{
+			"nvim-telescope/telescope-fzf-native.nvim",
+			build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release",
 		},
 	},
-	config = function(_, opts)
+	config = function()
 		local telescope = require("telescope")
 		local builtin = require("telescope.builtin")
+		local action_layout = require("telescope.actions.layout")
 
-		telescope.setup(opts)
+		local previewers = require("telescope.previewers")
+		local Job = require("plenary.job")
+		local new_maker = function(filepath, bufnr, opts)
+			filepath = vim.fn.expand(filepath)
+			Job:new({
+				command = "file",
+				args = { "--mime-type", "-b", filepath },
+				on_exit = function(j)
+					local mime_type = vim.split(j:result()[1], "/")[1]
+					if mime_type == "text" then
+						previewers.buffer_previewer_maker(filepath, bufnr, opts)
+					else
+						-- maybe we want to write something to the buffer here
+						-- vim.schedule(function()
+						-- 	vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, { "BINARY" })
+						-- end)
+					end
+				end,
+			}):sync()
+		end
+
+		telescope.setup({
+			extensions = {
+				["ui-select"] = {
+					require("telescope.themes").get_dropdown(),
+				},
+			},
+			fzf = {
+				fuzzy = true, -- false will only do exact matching
+				override_generic_sorter = true, -- override the generic sorter
+				override_file_sorter = true, -- override the file sorter
+				case_mode = "smart_case", -- or "ignore_case" or "respect_case"
+				-- the default case_mode is "smart_case"
+			},
+			defaults = {
+				buffer_previewer_maker = new_maker,
+				preview = false,
+				mappings = {
+					n = {
+						["cd"] = function(prompt_bufnr)
+							local selection = require("telescope.actions.state").get_selected_entry()
+							local dir = vim.fn.fnamemodify(selection.path, ":p:h")
+							require("telescope.actions").close(prompt_bufnr)
+							-- Depending on what you want put `cd`, `lcd`, `tcd`
+							vim.cmd(string.format("silent lcd %s", dir))
+							vim.api.nvim_set_current_dir(dir)
+						end,
+						["<M-p>"] = action_layout.toggle_preview,
+					},
+					i = {
+						["<M-p>"] = action_layout.toggle_preview,
+					},
+				},
+			},
+		})
+
+		telescope.load_extension("fzf")
 		telescope.load_extension("ui-select")
+		telescope.load_extension("file_browser")
 
 		vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
-		vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" }) -- needs ripgrep
+		vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
 		vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
 		vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
+
+		vim.keymap.set(
+			"n",
+			"<leader>fd",
+			":Telescope file_browser path=%:p:h select_buffer=true<CR>",
+			{ desc = "Telescope file browser" }
+		)
 	end,
 }
