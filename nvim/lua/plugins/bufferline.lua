ff524ddfd2c93acad0bfc5a32499edd8540f82a7
Mitutoyo <mitutoyum@proton.me>
2025-04-12 21:20:16 +0700

    more keymaps

--- nvim/lua/plugins/bufferline.lua
+++ nvim/lua/plugins/bufferline.lua
@@ -14,6 +14,11 @@ return {
 			"<cmd>BufferLineCyclePrev<CR>",
 			desc = "Previous Buffer",
 		},
+
+		{ "[B", "<cmd>BufferLineMovePrev<cr>", desc = "Move buffer prev" },
+
+		{ "]B", "<cmd>BufferLineMoveNext<cr>", desc = "Move buffer next" },
+
 		{
 			"<Tab>",
 			"<cmd>BufferLineCycleNext<CR>",
@@ -26,15 +31,33 @@ return {
 		},
 
 		{
-			"<leader>bd",
-			"<cmd>bd<CR>",
-			desc = "[B]uffer [D]elete",
+			"<leader>bco",
+			"<cmd>BufferLineCloseOthers<CR>",
+			desc = "Close Others",
 		},
 
 		{
-			"<leader>bco",
-			"<cmd>BufferLineCloseOthers<CR>",
-			desc = "[B]uffer [C]lose [O]thers",
+			"<leader>bp",
+			"<Cmd>BufferLineTogglePin<CR>",
+			desc = "Toggle Pin",
+		},
+
+		{
+			"<leader>bP",
+			"<Cmd>BufferLineGroupClose ungrouped<CR>",
+			desc = "Delete Non-Pinned Buffers",
+		},
+
+		{
+			"<leader>br",
+			"<Cmd>BufferLineCloseRight<CR>",
+			desc = "Delete Buffers to the Right",
+		},
+
+		{
+			"<leader>bl",
+			"<Cmd>BufferLineCloseLeft<CR>",
+			desc = "Delete Buffers to the Left",
 		},
 	},
 	opts = {
@@ -46,9 +69,18 @@ return {
 					text_align = "left",
 					separator = true,
 				},
+				{
+					filetype = "snacks_layout_box",
+				},
 			},
+			always_show_bufferline = false,
 			diagnostics = "nvim_lsp",
-			numbers = "buffer_id",
+			close_command = function(n)
+				Snacks.bufdelete(n)
+			end,
+			right_mouse_command = function(n)
+				Snacks.bufdelete(n)
+			end,
 		},
 	},
 }
