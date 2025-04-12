50990541dafa969e8d3cd59042556a0486cb9299
Mitutoyo <mitutoyum@proton.me>
2025-04-12 21:21:42 +0700

    change

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -17,9 +17,8 @@ return {
 			python = function(bufnr)
 				if require("conform").get_formatter_info("ruff_format", bufnr).available then
 					return { "ruff_format" }
-				else
-					return { "isort", "black" }
 				end
+				return { "isort", "black" }
 			end,
 
 			cpp = { "clang-format" },
