ffbec2ee84860cd73f3e2c34ddf08a069429bfb0
Mitutoyo <mitutoyum@proton.me>
2025-04-12 21:27:35 +0700

    added autoupdate autocmd

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -1,6 +1,15 @@
 vim.api.nvim_create_autocmd("TextYankPost", {
 	group = vim.api.nvim_create_augroup("yank_highlight", { clear = true }),
 	callback = function()
-		(vim.hl or vim.highlight).on_yank()
+		vim.hl.on_yank()
+	end,
+})
+
+vim.api.nvim_create_autocmd("VimEnter", {
+	group = vim.api.nvim_create_augroup("autoupdate", { clear = true }),
+	callback = function()
+		if require("lazy.status").has_updates then
+			require("lazy").update({ show = false })
+		end
 	end,
 })
